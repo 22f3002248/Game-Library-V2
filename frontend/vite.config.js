a307f58359b17501e56a86ef8e7ecb5aa2e8b98b
22f3002248 <22f3002248@ds.study.iitm.ac.in>
2025-04-26 07:22:16 +0530

    updates

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: true, // 👈 allows external access
+    hmr: {
+      protocol: 'wss', // or 'ws' if not using https
+      host: 'localhost',
+    },
+    allowedHosts: ['all'], // 👈 works in older Vite
+    // Fallback if that fails, use 'originIsAllowed' in connect middleware
+    originIsAllowed: () => true, // 👈 hacky workaround for new Vite
+  },
 })
